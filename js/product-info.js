0b928f27d132ece3992287af816aa11f53e0a5aa
Federico Maciel <federicomaciel.dev@gmail.com>
2022-09-07 21:33:05 -0300

    Product-info terminado

--- js/product-info.js
+++ js/product-info.js
@@ -4,8 +4,15 @@ let ProdInfo = [];
 let ProdComments = [];
 let imgContent = ""
 let htmlContent = "";
-let activeImage;
-let smallImages;
+let smallImages = [];
+let userComment = {
+     product: sessionStorage.getItem("ProdID"),
+     score: "",
+     description: "",
+     user: "",
+     dateTime: ""
+};
+let firstTime = true;
 
 
 // function getData(URL){
@@ -23,14 +30,22 @@ let smallImages;
 //      });
 // }
 
+function ImagesAnim(obj){
+     obj.forEach((img, i) => {
+     smallImages[i] = document.getElementById(`smImg${i}`);
+     });
+     for(let i=0;i<smallImages.length;i++){
+          smallImages[i].addEventListener("mouseover", function(e){
+               document.getElementById("ogImg").src = smallImages[i].src;
+          });
+     }
+}
+
 function showProductInfo(obj){
 //Muestra la información del producto usando el DOM
      obj.images.forEach((img, i) => {
-          if(i===0){
-               imgContent += `<img src="${img}" class="smImg active">`;
-          }else{
-               imgContent += `<img src="${img}" class="smImg">`;
-          }
+          imgContent += `<img src="${img}" id="smImg${i}">
+`;
      });
      htmlContent = `
      <div id="imagesContainer" class="w-75">
@@ -39,21 +54,37 @@ function showProductInfo(obj){
                ${imgContent}
           </div>
      </div>
-     <div id="ProdDescription" class="">
+     <div id="ProdDescription">
           <h2 class="row">${obj.category} > ${obj.name}</h2>
           <span class="row">${obj.description}</span>
           <span class="row d-flex align-items-center">${obj.currency} <h3 class="fs-4 col">${obj.cost}</h3></span>
           <span class="row">${obj.soldCount} vendidos hasta el momento.</span>
      </div>
      `;
+     //Botón de compra
+     //<div class="btn btn-primary row w-25">botón</div>
+     
      document.getElementById("Info").innerHTML = htmlContent;
 }
 
 function ShowProductComments(obj){
 //Muestra los comentarios y puntuaciones del producto mostrado.
+     document.getElementById("Comment").innerHTML = "";
      for(let comment of obj){
+          if(firstTime || comment.score > 0){
+               let stars = comment.score;
+               comment.score="";
+               for(let i=0; i<5; i++){
+                    if(i<stars){
+                         comment.score += `<span class="fa fa-star checked"></span>`
+                    }
+                    else{
+                         comment.score += `<span class="fa fa-star"></span>`
+                    }
+               }
+          }
           htmlContent = `
-               <div id="comentario" class="rounded mb-2 w-50">
+               <div id="comentario" class="rounded mb-2 w-100">
                     <span class="ps-3 pt-1 fs-5">${comment.user} - ${comment.score}</span>
                     <span class="ps-3 pt-1 text-muted">${comment.dateTime}</span>
                     <span class="ps-3 pt-1 fs-5">${comment.description}</span>
@@ -61,8 +92,55 @@ function ShowProductComments(obj){
           `;
           document.getElementById("Comment").innerHTML += htmlContent;
      }
+     firstTime = false;
 }
 
+function showUserCommOption(){
+     htmlContent = `
+     <div id="tuComentario" class="w-100">
+          <h2 id="title" class="fs-3 col">Escribe tu opinión</h2>
+          <div class="mb-3">
+               <label for="exampleFormControlTextarea1" class="form-label">Tu comentario</label>
+               <textarea id="com" class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Me encanto porque..."></textarea>
+          </div>
+          <select id="punt" class="form-select class="w-50" aria-label="Default select example">
+               <option selected hidden>Tu puntuación</option>
+               <option value="1">1</option>
+               <option value="2">2</option>
+               <option value="3">3</option>
+               <option value="4">4</option>
+               <option value="5">5</option>
+          </select>
+          <div class="col-12">
+               <button id="enviarCom" class="btn btn-primary mt-3" type="submit">Enviar opinión</button>
+          </div>
+     </div>
+     `;
+     document.getElementById("Comment").innerHTML += htmlContent;
+}
+
+function sendUserComment(obj){
+          let com = document.getElementById("com").value;
+          let punt = document.getElementById("punt").selectedIndex;
+          if(com == undefined || com == ""  || punt == undefined || punt == ""){
+               if((com == undefined || com == "") && (punt == undefined || punt == "")){
+                    alert("Debes ingresar una opinión y una puntuación!");
+               }else if(com == undefined || com == ""){
+                    alert("Debes ingresar una opinión!");
+               }else if(punt == undefined || punt == ""){
+                    alert("Debes ingresar una puntuación!");
+               }
+          }else{
+               let date = new Date();
+               userComment.description = com;
+               userComment.score = parseInt(punt);
+               userComment.user = localStorage.getItem("UserName");
+               userComment.dateTime = date.getFullYear() + '-' + (date.getMonth()+1)+ '-' + date.getDate() + ' ' + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+               obj.push(userComment);
+               localStorage.setItem("ThisPcComments", JSON.stringify(userComment));
+               location.reload();
+          }
+}
 
 
 document.addEventListener("DOMContentLoaded", function(e){
@@ -74,33 +152,30 @@ document.addEventListener("DOMContentLoaded", function(e){
                ProdInfo = resObj.data;
           }
           showProductInfo(ProdInfo);
-     });
-
-     getJSONData(COMMENT_URL).then(function (resObj) {
-          if (resObj.status === "ok") {
-               ProdComments = resObj.data;
-          }
-          //Verificamos si el localStorage cuenta con algún comentario puesto por el usuario en su pc.
-          if(localStorage.getItem("ThisPcComments") != undefined){
-               ProdComments += JSON.parse(localStorage.getItem("ThisPcComments"));
-          }
-          ShowProductComments(ProdComments);
-     });
-
-     let smallImages = document.getElementsByClassName("smImg");
-     let activeImage = document.getElementsByClassName("active");
-     for(let i=0;i<smallImages.length;i++){
-          smallImages[i].addEventListener("mouseover", function(e){
-               if(activeImage.length > 0){
-                    activeImage[0].classList.remove("active");
+          ImagesAnim(ProdInfo.images);
+     }).then(function(){
+          getJSONData(COMMENT_URL).then(function (resObj) {
+               if (resObj.status === "ok") {
+                    ProdComments = resObj.data;
                }
-               this.classList.add("active");
-               document.getElementById("ogImg").src = this.src;
+               //Verificamos si el localStorage cuenta con algún comentario puesto por el usuario en su pc.
+               if(localStorage.getItem("ThisPcComments") != null){
+                    ProdComments.push(JSON.parse(localStorage.getItem("ThisPcComments")));
+               }
+               ShowProductComments(ProdComments);
+               showUserCommOption();
+               
+               document.getElementById("enviarCom").addEventListener("click", function(e){
+                    if(localStorage.getItem("ThisPcComments") == null){
+                         sendUserComment(ProdComments);
+                    }else{
+                         if(confirm("Ya hiciste tu comentario! No se pueden hacer 2.\nQuieres eliminar tu comentario actual?")){
+                              localStorage.removeItem("ThisPcComments");
+                              location.reload();
+                         }
+                    }
+               });
           });
-     }
+     });
 
 });
-
-
-
-//Al agregar un comentario debo usar localStorage.setItem("", JSON.stringify(...)) para que quede bien guardado en el local storage
