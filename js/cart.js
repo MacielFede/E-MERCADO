7538351070954c557cd7760f9baecc00d53a8f91
Federico Maciel <federicomaciel.dev@gmail.com>
2022-10-17 19:21:44 -0300

    Seccion de costos lograda

--- js/cart.js
+++ js/cart.js
@@ -1,31 +1,57 @@
 const URL = CART_INFO_URL + "25801" + EXT_TYPE ;
 let products;
+let taxes = 0.15;
+let total = 0;
+let USDtoday;
+let myHeaders = new Headers();
+myHeaders.append("apikey", "S1UdVqa9oDhXDhf3ZbUHVJkoKcTO28FE");
+let requestOptions = {
+     method: 'GET',
+     redirect: 'follow',
+     headers: myHeaders
+};
+async function myFunction () {
+     //Usamos una api para buscar el valor del dolar en pesos uruguayos
+     const res = await fetch("https://api.apilayer.com/exchangerates_data/convert?to=UYU&from=USD&amount=1", requestOptions);
+     const data = await res.json();
+     USDtoday = data.result;
+}
+
 
 const showProducts = obj => {
 //Muestra los productos del json y los que el usuario agrego por su cuenta al mismo
      htmlContent = "";
-     for (let [index, product] of obj.articles.entries()) {
-          if(index > 0 && product.id != 50924 || index == 0){
-               htmlContent += `
-               <div class="row h-25 pe-0">
-                    <img src="${product.image}" alt="product image" class="col-md-4 col-sm-7" >
-                    <div class="col-sm-6 col-md-4 d-grid" style="height:fit-content">
-                         <span class="fw-bold" id="hola">${product.name}</span>
-                         <span>Cantidad: <input id="i${index}" onInput="subtotal(${index})" style="width: 3rem" type="number" value="${product.count}" min="1"> </span>
-                         <a style="width: fit-content" onClick="deleteProd(${index})" href=#>Eliminar producto</a>
-                    </div>
-                    <div class="allPrice col pe-0 text-end">
-                         <span class="unitPrice">${product.currency}${product.unitCost} - </span><strong id="cost-${index}">${product.currency}${product.unitCost}</strong>
-                    </div>
-               </div>
-               <hr class="mt-3">
-               `;
-          }
-     }
-     document.getElementById("prodList").innerHTML = htmlContent;
+     total = 0;
      if(products.articles.length == 0){
           document.getElementById("delivery").innerHTML = "<strong>Esto esta vació!</strong>"
+     }else{
+          for (let [index, product] of obj.articles.entries()) {
+               if(index > 0 && product.id != 50924 || index == 0){
+                    //Esta validacion es usada para saber si el usuario agrego al carrito el auto peugot 208
+                    htmlContent += `
+                    <div class="row h-25 pe-0">
+                         <img src="${product.image}" alt="product image" class="col-md-4 col-sm-7" >
+                         <div class="col-sm-6 col-md-4 d-grid" style="height:fit-content">
+                              <span class="fw-bold" id="hola">${product.name}</span>
+                              <span>Cantidad: <input id="i${index}" onkeydown="return false" onInput="subtotal(${index})" style="width: 3rem" type="number" value="${product.count}" min="1"> </span>
+                              <a style="width: fit-content" onClick="deleteProd(${index})" href=#>Eliminar producto</a>
+                         </div>
+                         <div class="allPrice col pe-0 text-end">
+                              <span class="unitPrice">${product.currency}${product.unitCost} - </span><strong id="cost-${index}">${product.currency}${product.unitCost}</strong>
+                         </div>
+                    </div>
+                    <hr class="mt-3">
+                    `;
+                    if(product.currency === "USD"){
+                         total += product.count * product.unitCost;
+                    }else{
+                         total += Math.round(product.count * (product.unitCost / USDtoday));
+                    }
+               }
+          }
+          updateCosts();
      }
+     document.getElementById("prodList").innerHTML = htmlContent;
 }
 
 function subtotal(index){
@@ -33,6 +59,8 @@ function subtotal(index){
      let count = document.getElementById(`i${index}`).value;
      products.articles[index].count = count;
      document.getElementById(`cost-${index}`).innerHTML = `${products.articles[index].currency}${products.articles[index].unitCost * count}`;
+     total += products.articles[index].unitCost
+     updateCosts();
 }
 
 function deleteProd(index){
@@ -67,18 +95,43 @@ function deleteProd(index){
      showProducts(products);
 }
 
+function updateCosts(){
+     document.getElementById("costText").innerHTML = "USD" + total.toLocaleString('en-US');
+     document.getElementById("taxText").innerHTML = "USD" + Math.round(total*taxes).toLocaleString('en-US');
+     document.getElementById("totalCostText").innerHTML = "USD" + Math.round((total*taxes) + total).toLocaleString('en-US');
+}
+
+
 document.addEventListener("DOMContentLoaded", e =>{
-     getJSONData(URL).then(function(resObj){
-          if(resObj.status === "ok"){
-               products = resObj.data;
-               if(localStorage.getItem("cartProducts") != null){
-                    products.articles = products.articles.concat(JSON.parse(localStorage.getItem("cartProducts")));
+     
+     myFunction().then(e =>{
+          getJSONData(URL).then(function(resObj){
+               if(resObj.status === "ok"){
+                    products = resObj.data;
+                    if(localStorage.getItem("cartProducts") != null){
+                         products.articles = products.articles.concat(JSON.parse(localStorage.getItem("cartProducts")));
+                    }
                }
-          }
-          showProducts(products);
-     });
+               showProducts(products);
+          });
+     })
 
      document.getElementsByClassName("btn")[0].addEventListener("click", function(){
           document.getElementById("delivery").classList.add("was-validated");
+     });
+
+     document.getElementById("premium").addEventListener("change", function(){
+          taxes = 0.15;
+          updateCosts();
+     });
+
+     document.getElementById("express").addEventListener("change", function(){
+          taxes = 0.07;
+          updateCosts();
+     });
+
+     document.getElementById("standard").addEventListener("change", function(){
+          taxes = 0.05;
+          updateCosts();
      });
 });
