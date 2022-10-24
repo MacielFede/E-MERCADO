3adc3cbfa0659c966bebed8aa2b909c24a1a678b
Federico Maciel <federicomaciel.dev@gmail.com>
2022-10-24 12:28:11 -0300

    entrega 6 v1.0

--- js/cart.js
+++ js/cart.js
@@ -3,9 +3,17 @@ let products;
 let taxes = 0.15;
 let total = 0;
 let USDtoday;
+let ccNumber = document.getElementById("ccNumber");
+let ccSecurity = document.getElementById("ccSecurity");
+let ccExpiry = document.getElementById("ccExpiry");
+let creditCardRadioB = document.getElementById("credit");
+let transferRadioB = document.getElementById("transfer");
+let bankAcountNumber = document.getElementById("bankNumber");
+
 let myHeaders = new Headers();
 myHeaders.append("apikey", "S1UdVqa9oDhXDhf3ZbUHVJkoKcTO28FE");
 let requestOptions = {
+     //Seteo los argumentos para pasarle al fetch de myfunction()
      method: 'GET',
      redirect: 'follow',
      headers: myHeaders
@@ -14,7 +22,18 @@ async function myFunction () {
      //Usamos una api para buscar el valor del dolar en pesos uruguayos
      const res = await fetch("https://api.apilayer.com/exchangerates_data/convert?to=UYU&from=USD&amount=1", requestOptions);
      const data = await res.json();
-     USDtoday = data.result;
+     if(data.success){
+          USDtoday = data.result;
+     }else{
+          //alert("El servicio de cambio de moneda dejo de funcionar. Por favor deje un mail en federicomaciel.dev@gmail.com indicando el problema");
+          USDtoday = 43;
+     }
+}
+
+const switchClasses = (classToAdd, classToDelete, elementId)=>{
+     //Dado un elemento, agrega una clase y elimina otra clase.
+     document.getElementById(elementId).classList.add(classToAdd);
+     document.getElementById(elementId).classList.remove(classToDelete);
 }
 
 
@@ -37,7 +56,7 @@ const showProducts = obj => {
                               <a style="width: fit-content" onClick="deleteProd(${index})" href=#>Eliminar producto</a>
                          </div>
                          <div class="allPrice col pe-0 text-end">
-                              <span class="unitPrice">${product.currency}${product.unitCost} - </span><strong id="cost-${index}">${product.currency}${product.unitCost}</strong>
+                              <span class="unitPrice">${product.currency}${product.unitCost} - </span><strong id="cost-${index}">${product.currency}${(product.unitCost * product.count)}</strong>
                          </div>
                     </div>
                     <hr class="mt-3">
@@ -55,12 +74,10 @@ const showProducts = obj => {
 }
 
 function subtotal(index){
-//Actualiza el subtotal del producto donde cambia la cantidad
-     let count = document.getElementById(`i${index}`).value;
-     products.articles[index].count = count;
-     document.getElementById(`cost-${index}`).innerHTML = `${products.articles[index].currency}${products.articles[index].unitCost * count}`;
-     total += products.articles[index].unitCost
-     updateCosts();
+//Actualiza el subtotal del producto donde cambia la cantidad y reimprime el arreglo
+     products.articles[index].count = document.getElementById(`i${index}`).value;
+     localStorage.setItem("cartProducts", JSON.stringify(products.articles));
+     showProducts(products);
 }
 
 function deleteProd(index){
@@ -101,10 +118,64 @@ function updateCosts(){
      document.getElementById("totalCostText").innerHTML = "USD" + Math.round((total*taxes) + total).toLocaleString('en-US');
 }
 
+function paymentValidity(){
+     if(!transferRadioB.checked && !creditCardRadioB.checked){
+          switchClasses("d-block", "d-none", "selectPayment");
+          switchClasses("is-invalid", "is-valid", "credit");
+          switchClasses("is-invalid", "is-valid", "transfer");
+          return false;
+     }else{
+          switchClasses("d-none", "d-block", "selectPayment");
+          switchClasses("is-valid", "is-invalid", "credit");
+          switchClasses("is-valid", "is-invalid", "transfer");
+          return paymentValidityInfo();
+     }
+}
 
-document.addEventListener("DOMContentLoaded", e =>{
-     
-     myFunction().then(e =>{
+function paymentValidityInfo(){
+     if(transferRadioB.checked){
+          //Si la casilla de transferencia bancaria esta chequeada, debo verificar si tiene toda la info
+          if(bankAcountNumber.value == ""){
+               switchClasses("is-invalid", "is-valid", "bankNumber");
+               switchClasses("d-block", "d-none", "givePaymentInfo");
+               return false;
+          }else{
+               switchClasses("is-valid", "is-invalid", "bankNumber");
+               switchClasses("d-none", "d-block", "givePaymentInfo");
+               return true;
+          }
+     }else{
+          //Significa que la casilla de credit card esta chequeada, verifico si tengo toda la informacion
+          if(ccNumber.value == "" || ccSecurity.value == "" || ccExpiry.value == ""){
+               if(ccNumber.value == ""){
+                    switchClasses("is-invalid", "is-valid", "ccNumber");
+               }else{
+                    switchClasses("is-valid", "is-invalid", "ccNumber");
+               }
+               if(ccSecurity.value == ""){
+                    switchClasses("is-invalid", "is-valid", "ccSecurity");
+               }else{
+                    switchClasses("is-valid", "is-invalid", "ccSecurity");
+               }
+               if(ccExpiry.value == ""){
+                    switchClasses("is-invalid", "is-valid", "ccExpiry");
+               }else{
+                    switchClasses("is-valid", "is-invalid", "ccExpiry");
+               }
+               switchClasses("d-block", "d-none", "givePaymentInfo");
+               return false;
+          }else{
+               switchClasses("d-none", "d-block", "givePaymentInfo");
+               switchClasses("is-valid", "is-invalid", "ccNumber");
+               switchClasses("is-valid", "is-invalid", "ccSecurity");
+               switchClasses("is-valid", "is-invalid", "ccExpiry");
+               return true;
+          }
+     }
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+     myFunction().then(() =>{
           getJSONData(URL).then(function(resObj){
                if(resObj.status === "ok"){
                     products = resObj.data;
@@ -134,4 +205,42 @@ document.addEventListener("DOMContentLoaded", e =>{
           taxes = 0.05;
           updateCosts();
      });
+     
+     creditCardRadioB.addEventListener("click", function(){
+          ccNumber.disabled = false;
+          ccSecurity.disabled = false;
+          ccExpiry.disabled = false;
+          bankAcountNumber.disabled = true;
+          transferRadioB.checked = false;
+          bankAcountNumber.className = "num form-control";
+          document.getElementById("payment").innerHTML = `Tarjeta de crédito <a href="" data-bs-toggle="modal" data-bs-target="#paymentModal">Cambiar forma de pago</a>`;
+     });
+     transferRadioB.addEventListener("click", function(){
+          ccNumber.disabled = true;
+          ccSecurity.disabled = true;
+          ccExpiry.disabled = true;
+          bankAcountNumber.disabled = false;
+          creditCardRadioB.checked = false;
+          ccExpiry.className = "form-control";
+          ccNumber.className = "num form-control";
+          ccSecurity.className = "num form-control";
+          document.getElementById("payment").innerHTML = `Transferencia bancaria <a href="" data-bs-toggle="modal" data-bs-target="#paymentModal">Cambiar forma de pago</a>`;
+     });
+     document.getElementById("paymentModal").addEventListener("keyup", function(){
+          if(paymentValidity()){
+               paymentValidityInfo();
+          }     
+     });
+
+     document.getElementById("delivery").addEventListener('submit', function (event) {
+          console.log(!paymentValidity());
+          console.log(!this.checkValidity());
+          if (!paymentValidity() || !this.checkValidity()) {
+               event.preventDefault();
+               event.stopPropagation();
+          }else{
+               alert("compraste pa");
+          }
+          this.classList.add('was-validated')
+     }, false);
 });
