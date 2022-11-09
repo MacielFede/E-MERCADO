48e4764be9c5020011c494e45866587f47845bf5
Federico Maciel <federicomaciel.dev@gmail.com>
2022-11-09 12:00:28 -0300

    Arreglos en UI

--- js/product-info.js
+++ js/product-info.js
@@ -61,6 +61,15 @@ function showProductInfo(obj){
                     </div>`;
                }
           });
+          imgContent+=`
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+               <span class="carousel-control-next-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">Next</span>
+          </button>`;
      }else{
           imgContent = `
           <img src="${obj.images[0]}" id="ogImg" class="w-75">
